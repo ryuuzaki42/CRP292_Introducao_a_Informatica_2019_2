1a09953e215fc251ce24c966873a79a93b21af5f
João D. Batista <joao42lbatista@gmail.com>
2020-02-18 11:34:17 -0300

    Update crp292Aula01ApresentacaoDisciplina.pdf, crp292Links.r, and crp292ProgramaAnalitico.pdf

---
+++ aulas/aula01/crp292Links.r
@@ -0,0 +1,29 @@
+## Introdução à Informática - Playlist no Youtube
+https://www.youtube.com/playlist?list=PLtiJyyRsEC2oWJNfNZ1McCai5mJTHp7r6
+
+## Libreoffice instalação
+https://pt-br.libreoffice.org/ajuda/instrucoes-de-instalacao/
+https://www.libreoffice.org/download/portable-versions/
+https://pt-br.libreoffice.org/baixe-ja/libreoffice-novo/
+https://www.libreoffice.org/download/download/
+
+## Guia de funções no Calc
+https://help.libreoffice.org/4.4/Calc/Spreadsheet_Functions/pt-BR
+
+## Lista de expressões regulares
+https://help.libreoffice.org/4.4/Common/List_of_Regular_Expressions/pt-BR
+
+## Guia geral Online
+https://help.libreoffice.org/latest/pt-BR/text/shared/05/new_help.html
+
+## Guia geral Offline
+Aperte F1 ou menu Ajuda e depois Ajuda do Libreoffice
+
+## Slack
+https://slack.com/intl/pt-br/features
+
+## Virtualbox downloads
+https://www.virtualbox.org/wiki/Downloads
+
+## OSBoxes Virtualbox images
+https://www.osboxes.org/virtualbox-images/
